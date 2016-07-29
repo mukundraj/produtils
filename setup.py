11d406c5c28ccd5e9a8c0a3ed58e4b2821241eaa
mukundraj <mraj@cs.utah.edu>
2016-07-28 23:58:22 -0600

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='produtils',
+      version='0.1',
+      description='A nifty library of productivity tools',
+      url='http://github.com/storborg/funniest',
+      author='Mukund Raj',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['produtils'],
+      zip_safe=False)
